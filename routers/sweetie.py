45adac7f33064ceb98f70a69493307bd65158571
aishuchen <42426468+aishuchen@users.noreply.github.com>
2021-03-16 17:40:41 +0800

    Develop (#1)
    * update: 一些优化
    
    * update: 菜品の优化
    
    * update: 优化一个标点符号
    
    Co-authored-by: huoyue.jiang <huoyue.jiang@mail.hypers.com>

--- routers/sweetie.py
+++ routers/sweetie.py
@@ -5,6 +5,10 @@ router = APIRouter(prefix="/api")
 
 
 @router.get("/rechoose")
-async def re_choose():
-    await sweetie.re_choose()
-    return {"message": "已重新选择，消息已推送"}
+def re_choose():
+    item = sweetie.check_choose_time()
+    if item[0]:
+        sweetie.re_choose()
+        return {"message": "已重新选择，消息已推送"}
+    else:
+        return {"message": "没到饭点不要瞎玩哦，饿了多喝热水 ^_^"}
--- utility/sweetie.py
+++ utility/sweetie.py
@@ -1,39 +1,49 @@
 import datetime
 import const
 import random
-
 from utility.wx_work import messenger
+from .tz import Asia_Shanghai
 
 re_choose_url = f"{const.site}/rechoose"
 
 
+def get_choices():
+    for direction in const.choices:
+        for foods in const.choices["direction"]:
+            yield (direction, foods)
+
+
 def choose():
-    direction = random.choice(tuple(const.choices.keys()))
-    food = random.choice(const.choices[direction])
+    food_choices = list(get_choices())
+    direction, food = random.choices(food_choices)
     return direction, food
 
 
 def serve():
+    item = check_choose_time()
+    if not item[0]:
+        return 
+
+    time_map = {
+        "AM": "午",
+        "PM": "晚"
+    }
+    time = time_map[item[1]]
     direction, food = choose()
     push(direction=direction, food=food)
-    return food
+    return direction, food 
 
 
-async def re_choose():
+def re_choose(time):
     direction, food = choose()
     push(direction=direction, food=food, rechoose=True)
-    return food
+    return direction, food
 
 
 def push(direction, food, time="", rechoose=False):
-    now = datetime.datetime.now()
-    if 11 <= now.hour <= 12:
-        time = "午"
-    elif 18 <= now.hour <= 19:
-        time = "晚"
     prefix = f"马上到吃{time}饭时间啦，今日建议"
     if rechoose:
-        prefix = "已重新选择, 建议"
+        prefix = "已重新选择，建议"
     msg = (
         f'{prefix}<font color="info">{direction}</font>的<font color="info">{food}</font>\n'
         f'还可以：\n'
@@ -41,3 +51,12 @@ def push(direction, food, time="", rechoose=False):
     )
     messenger.send(msg)
 
+
+def check_choose_time():
+    now = datetime.datetime.now(tz=Asia_Shanghai)
+    if 11 <= now.hour <= 12:
+        return True, "AM"
+    elif 18 <= now.hour <= 19:
+        return True, "PM"
+    else:
+        return False, ""
